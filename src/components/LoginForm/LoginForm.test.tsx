287cdaec06070d8938b2d317fd81d1dfd5fd4f59
RomanParaskiva <parasta2018@gmail.com>
2022-09-06 16:23:40 +0300

    test and stories for login form

--- src/components/LoginForm/LoginForm.test.tsx
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -1,1 +1,38 @@
-export {};
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import { LoginForm } from "./LoginForm";
+
+describe("test LoginForm", () => {
+    test("renders LoginForm component", async () => {
+        render(<LoginForm />);
+
+        expect(await screen.findByText(/start/)).not.toBeNull();
+    });
+
+    test("LoginForm snapshot", () => {
+        const { container } = render(<LoginForm />);
+
+        expect(container).toMatchSnapshot();
+    })
+
+    const setup = () => {
+        const utils = render(<LoginForm />)
+        const input = utils.getByLabelText('userName')
+        return {
+          input,
+          ...utils,
+        }
+      }
+
+    test("test input change", async () => {
+        render(<LoginForm />);
+
+        const input = screen.getByTestId("userName");
+
+        await fireEvent.input(input, { target: { value: "Roman" }});
+
+        await waitFor(() => expect( input ).toHaveDisplayValue("Roman"))
+        
+    })
+});
--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
 
 export const LoginForm = () => {
+  const [name, setName] = useState('');
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(() => e.target.value);
+  }
+
+  const handleStart = () => {
+    if (name.length > 3) {
+      localStorage.setItem('userName', name)
+    }
+  }
+
+
   return (
-    <div className="flex w-100 h-full justify-center items-center">
-      <div>
-        <form>
-          <label>
+    <div className="flex w-100 py-20 justify-center items-center">
+      <div className='bg-gray-200 rounded p-3'>
+        <form onSubmit={(e) => e.preventDefault()} className='flex flex-col gap-5'>
+          <label className='text-center font-semibold text-lg'>
             Ваше имя
           </label>
-          <input type="text" name="userName" />
-
-          <button className="p-3 bg-green-400 rounded-md text-white uppercase ">
+            <input
+              data-testid="userName"
+              className='outline-none text-black bg-transparentborder-gray-600 rounded-md border p-1'
+              type="text"
+              value={name}
+              name="userName"
+              aria-label="userName"
+              onChange={handleInput}
+            />
+          
+          <button
+            className="p-3 bg-green-400 rounded-md text-white uppercase "
+            onClick={handleStart}
+          >
             start
           </button>
         </form>
