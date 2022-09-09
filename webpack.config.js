4e934b725d1dfc666f54f10902fdcbfb2c7140ca
RomanParaskiva <parasta2018@gmail.com>
2022-09-09 14:53:02 +0300

    fix wp config

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === 'production'
 
 const config = {
-  entry: "./src/index.tsx",
-  mode: "development",
+  entry: './src/index.tsx',
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.(js|ts)x?$/,
-        loader: require.resolve("babel-loader"),
+        loader: require.resolve('babel-loader'),
         exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader", "postcss-loader"],
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
+        type: 'asset',
       },
     ],
   },
-  resolve: { extensions: ["*", ".js", ".jsx", ".ts", ".tsx"] },
+  resolve: { extensions: ['*', '.js', '.jsx', '.ts', '.tsx'] },
   output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "./index.js",
-    publicPath: "/",
-    globalObject: "(typeof self!='undefined'?self:global)",
+    path: path.join(__dirname, '/dist'),
+    filename: './index.js',
+    publicPath: '/',
+    globalObject: '(typeof self!="undefined"?self:global)',
   },
   devServer: {
     historyApiFallback: true,
@@ -39,16 +39,16 @@ const config = {
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
   ],
-};
+}
 
 module.exports = () => {
   if (isProduction) {
-    config.mode = "production";
+    config.mode = 'production'
   } else {
-    config.mode = "development";
+    config.mode = 'development'
   }
-  return config;
-};
+  return config
+}
