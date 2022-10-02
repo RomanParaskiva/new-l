d71aaacb18c209c939dac1e392cb922f9e673c64
Roman Paraskiva <parasta2018@gmail.com>
2022-10-02 12:19:46 +0300

    hw-8 done

--- hw-8/immutability.test.ts
+++ hw-8/immutability.test.ts
@@ -1,4 +1,10 @@
-import { OriginalTeam, ExpectedTeam } from "./immutability";
+import {
+  OriginalTeam,
+  ExpectedTeam,
+  originalTeamToExpectedTeamDeep,
+  originalTeamToExpectedTeam,
+  originalArrayToExpectedArray,
+} from "./immutability";
 
 // Задание 1
 test("team to team", () => {
@@ -14,7 +20,7 @@ test("team to team", () => {
     roster: 25,
   };
 
-  expect(originalTeamToExpectedTeam(originalTeam)).toBe(expectedTeam);
+  expect(originalTeamToExpectedTeam(originalTeam)).toEqual(expectedTeam);
 });
 
 // Задание 2
@@ -23,7 +29,7 @@ test("array to array", () => {
 
   const expectedArray = ["two", 3, 4, 5];
 
-  expect(originalArrayToExpectedArray(originalArray)).toBe(expectedArray);
+  expect(originalArrayToExpectedArray(originalArray)).toEqual(expectedArray);
 });
 
 // Задание 3
@@ -44,5 +50,5 @@ test("team to team deep", () => {
     },
   };
 
-  expect(originalTeamToExpectedTeam(originalTeam)).toBe(expectedTeam);
+  expect(originalTeamToExpectedTeamDeep(originalTeam)).toEqual(expectedTeam);
 });
