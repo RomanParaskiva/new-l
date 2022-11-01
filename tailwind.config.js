889c39ee58b2cf50a041ddcd5cdfb797f544270e
Roman Paraskiva <parasta2018@gmail.com>
2022-11-01 14:54:56 +0300

    added readme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      "background-color": "#85FFBD",
+      "background-image": "linear-gradient(294deg, #85FFBD 0%, #FFFB7D 100%)",
+    },
   },
   plugins: [],
-}
+};
